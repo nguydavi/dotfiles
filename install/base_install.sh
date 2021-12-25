56f924e43e3e05c7df586c6e8e1dd8c09689536f
David Nguyen <david2456@gmail.com>
2021-12-25 14:20:32 +0000

    base_install: Install go binaries for vim-go

--- install/base_install.sh
+++ install/base_install.sh
@@ -76,6 +76,8 @@ clone_repo https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install Vim plugins
 vim +PluginInstall +qall
+# Install Go dependencies for vim-go
+vim +GoInstallBinaries +qall
 
 # fzf
 clone_repo https://github.com/junegunn/fzf.git ~/.fzf
