6c01f67d65a7c293ef352c353954bce9b951bf32
David Nguyen <david2456@gmail.com>
2025-10-04 20:27:22 +0200

    install_mac_os: Update tools and LSPs to be installed

--- install/install_mac_os.sh
+++ install/install_mac_os.sh
@@ -5,24 +5,39 @@ source ./install/base_install.sh
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Installing tools
 /opt/homebrew/bin/brew install \
     ag \
+    awscli \
     basictex \
     cmake \
     glances \
+    go \
+    ipython \
     jq \
     lsd \
-    ripgrep \
     termshark \
+    tig \
+    tree \
     tmux
 
 /opt/homebrew/bin/brew install --cask cameracontroller
 
+# Rustup & Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 # Install tmux plugins
 ~/.tmux/plugins/tpm/bin/install_plugins
 
+# Installing LSPs
+/opt/homebrew/bin/brew install terraform-ls
+
 pip install Pygments
 pip install "python-lsp-server[all]"
+
+go install golang.org/x/tools/gopls@latest
+
+rustup component add rust-analyzer
 
 echo_green "All done!"
 echo "Make sure to install the Nerd font 'FantasqueSansMono' https://github.com/ryanoasis/nerd-fonts/releases"
