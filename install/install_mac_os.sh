e3bbd7bddf1f5d951a0e47d45cc56ee555b2c2c6
David Nguyen <david2456@gmail.com>
2026-02-16 09:53:40 +0100

    Re-enable git maintenance
    The 'none' strategy was not always recognized and the lock issue does
    not happen anymore.

--- install/install_mac_os.sh
+++ install/install_mac_os.sh
@@ -86,8 +86,6 @@ install_symlink ~/Library/Application\ Support/com.mitchellh.ghostty/config ${PW
 mkdir -p ~/.config/karabiner
 install_symlink ~/.config/karabiner/karabiner.json ${PWD}/karabiner.json
 install_symlink ~/.config/jj/config.toml ${PWD}/jjconfig.toml
-## To avoid issues like https://github.com/jj-vcs/jj/issues/7210
-git config --global maintenance.strategy none
 install_symlink ~/.config/ccstatusline/settings.json ${PWD}/ccstatusline_settings.json
 
 # Ensure we have the latest version of vim installed
